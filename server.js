cd7d44550dde20001498e832dfda1349762c4d61
Annisa Purbandari <a.purbandari@gmail.com>
2020-12-17 14:40:49 +1100

    added serviceworker and webmanifest

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+const PWD = process.env.DB_PWD;
 const databaseUrl = `mongodb+srv://annisa:${encodeURIComponent(PWD)}@cluster0.l77tg.mongodb.net/funds`;
 
 mongoose.connect(databaseUrl, {
